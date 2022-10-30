5771f56d2031862272964b0ab3ec5ec64a7a2517
Vasilya <lady_drakula_94@mail.ru>
2022-10-30 22:44:24 +0500

    Ex написала программу

--- Ex/Program.cs
+++ Ex/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+string[] array = { "Hello", "2", "world", ":-)" };
+int size = array.Length;
+int count = 0;
+
+for (int i = 0; i < size; i++)
+{
+    if (array[i].Length <= 3)
+    {
+        count++;
+    }
+}
+
+string[] newarray = new string[count];
+int j = 0;
+for (int i = 0; i < size; i++)
+{
+    if (array[i].Length <= 3)
+    {
+        if (j < count)
+        {
+            newarray[j] = array[i];
+            Console.Write(newarray[j]+", ");
+        } 
+        j++;       
+    }    
+}
+
